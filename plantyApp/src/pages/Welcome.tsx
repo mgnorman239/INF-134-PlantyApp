89422d0879bfab9db607a9c84f1ac2fa91cd7d65
John Doe <viktorsworkmail@gmail.com>
2020-02-13 18:31:00 -0800

    edit buttons in welcome page

--- plantyApp/src/pages/Welcome.tsx
+++ plantyApp/src/pages/Welcome.tsx
@@ -1,9 +1,11 @@
 import React ,{ Component }from 'react';
 import { IonContent, IonHeader, IonPage, IonInput, IonItem, IonButton, IonIcon, IonToolbar, IonTitle, IonButtons, IonBackButton, IonText } from '@ionic/react';
-import { mail, lockClosed, arrowBack} from 'ionicons/icons';
+import { mail, lockClosed, arrowBack, construct} from 'ionicons/icons';
 import "./Welcome.css"
+import { SSL_OP_CIPHER_SERVER_PREFERENCE } from 'constants';
 
-const Welcome: React.FC = () => {
+const Welcome: React.FC=()=> {
+
     return (
         <IonPage>
         <IonHeader>
@@ -15,11 +17,18 @@ const Welcome: React.FC = () => {
             </IonToolbar>
         </IonHeader>
         <IonContent>
-        <IonItem class="center-plant">
+                    
+                <IonItem class="center-plant">
                     <img src={require("./images/Planty.jpg")} alt="Planty the Self-Care Plant"></img>
                 </IonItem>
+        <IonButton class = "previous" color= "#8388C8"> Prev</IonButton>
+        <IonButton class = "Actuvity-button" color="#8388C8">Activity</IonButton>
+        <IonButton class = "next" color= "#8388C8"> next</IonButton>
         </IonContent>
         </IonPage>
+
     )
+    
 }
+
 export default Welcome;
