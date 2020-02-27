76d8eafa611851887391476835d2348f40dcee3b
John Doe <viktorsworkmail@gmail.com>
2020-02-27 12:45:30 -0800

    added slide function for PickActivity grids, but need to fix button

--- plantyApp/src/pages/PickActivity.tsx
+++ plantyApp/src/pages/PickActivity.tsx
@@ -1,13 +1,25 @@
-import { camera, trash, close } from 'ionicons/icons';
+import { camera, trash, close, arrowDownOutline, arrowUpOutline } from 'ionicons/icons';
 import React ,{ Component }from 'react';
 import {IonSlides, IonSlide ,IonListHeader,IonLabel,IonRadio, IonContent, IonHeader, IonPage, IonInput, IonItem, IonButton, IonIcon, IonToolbar, IonTitle, IonButtons, IonBackButton, IonText, IonSelect, IonRadioGroup, IonGrid, IonRow, IonCol } from '@ionic/react';
 import { mail, lockClosed, arrowBack, construct} from 'ionicons/icons';
 import { render } from '@testing-library/react';
 // import "./Welcome.css"
 
-
+const slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    direction:'vertical'
+  };
 
 class  PickActivity extends React.Component{
+
+    grid_nextPage=()=>{
+        
+    }
+    grid_lastPage=()=>{
+
+    }
+
     render(){
         return(<IonPage>
             <IonHeader>
@@ -19,6 +31,8 @@ class  PickActivity extends React.Component{
             </IonToolbar>
         </IonHeader>
         <IonContent>
+        <IonSlides class="activityPick" pager={false} options={slideOpts}>
+            <IonSlide>
             <IonGrid class="grid_menu">
                 {/* grid be wider have to use br element */}
                 {/* size is for the*/}
@@ -57,12 +71,78 @@ class  PickActivity extends React.Component{
                             <IonIcon icon={mail} class="input-line-icon"></IonIcon>
                         </IonCol>
                         <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon" color="black"></IonIcon>
+                        </IonCol>
+                    </IonRow>
+                    <br></br>
+                    <br></br>
+                    <IonRow>
+                    
+                        <IonButton class="next_page" onClick={this.grid_nextPage}>
+                            <IonIcon icon={arrowDownOutline}></IonIcon>
+                            next page
+                        </IonButton>
+                    </IonRow>
+            </IonGrid>
+            </IonSlide>
+            <IonSlide>
+            
+            <IonGrid class="grid_menu">
+                {/* grid be wider have to use br element */}
+                {/* size is for the*/}
+                <IonRow>
+                    
+                        <IonButton class="next_page" onClick={this.grid_lastPage}>
+                            <IonIcon icon={arrowUpOutline}></IonIcon>
+                            lst page
+                        </IonButton>
+                </IonRow>
+
+                <IonRow class="grid_row">
+                    <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon" ></IonIcon>
+                        </IonCol>
+                        <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon" ></IonIcon>
+                        </IonCol>
+                        
+                </IonRow>
+                    <br></br>
+                    <br></br>
+                    <IonRow class="grid_row">
+                    <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon"></IonIcon>
+                        </IonCol>
+                        <IonCol>
                             <IonIcon icon={mail} class="input-line-icon"></IonIcon>
                         </IonCol>
                     </IonRow>
-                    
+                    <br></br>
+                    <br></br>
+                    <IonRow>
+                    <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon"></IonIcon>
+                        </IonCol>
+                        <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon"></IonIcon>
+                        </IonCol>
+                    </IonRow>
+                    <br></br>
+                    <br></br>
+                    <IonRow >
+                    <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon"></IonIcon>
+                        </IonCol>
+                        <IonCol>
+                            <IonIcon icon={mail} class="input-line-icon" color="black"></IonIcon>
+                        </IonCol>
+                    </IonRow>
+                    <br></br>
+                    <br></br>
                     
             </IonGrid>
+            </IonSlide>
+            </IonSlides>
         </IonContent>
         </IonPage>
         ) 
